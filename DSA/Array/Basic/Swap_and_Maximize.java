2ed8fd73ce686d7df12ee28f69dc329734093b6b
samarth bahirgonde <samarthnbahirgonde49@gmail.com>
2023-08-09 12:21:33 +0530

    Swap and Maximize gfg problem solution

---
+++ DSA/Array/Basic/Swap_and_Maximize.java
@@ -0,0 +1,14 @@
+
+class Swap_and_Maximize
+{
+    long maxSum(long arr[] ,int n)
+    {
+        Arrays.sort(arr);
+        long sum = 0;
+        for(int i = 0; i < n/2; i++){
+            sum = sum - (2* arr[i]);
+            sum = sum + (2* arr[n-i-1]);
+        }
+        return sum;
+    }
+}
