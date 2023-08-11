3e62a6ca1abda4c6269e6b0fcc79bb11b0023c49
samarth bahirgonde <samarthnbahirgonde49@gmail.com>
2023-08-11 20:06:48 +0530

    Searching_in_an_array_where_adjacent_differ_by_at_most_k Question solution

---
+++ DSA/Array/Easy/Searching_in_an_array_where_adjacent_differ_by_at_most_k.java
@@ -0,0 +1,12 @@
+class Complete{
+    
+    public static int search (int arr[], int n, int x, int k) {
+        for(int i = 0; i< n; i++){
+            if(arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+}
