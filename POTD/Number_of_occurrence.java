e01b204d9eef1f3c5ca05c50274205d7e4d94a53
samarth bahirgonde <samarthnbahirgonde49@gmail.com>
2023-08-20 12:24:32 +0530

    Number_of_occurrence POTD

---
+++ POTD/Number_of_occurrence.java
@@ -0,0 +1,10 @@
+class Number_of_occurrence{
+    int count(int[] arr, int n, int x) {
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i]==x) cnt++;
+        }
+        
+        return cnt;
+    }
+}
