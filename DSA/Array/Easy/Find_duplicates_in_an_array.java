0ac8e7ab241f5c03c05bc931face69a5f25010ee
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-08-25 14:22:28 +0530

    Find duplicates in an array

---
+++ DSA/Array/Easy/Find_duplicates_in_an_array.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Find_duplicates_in_an_array{
+    class Solution {
+    public static ArrayList<Integer> duplicates(int arr[], int n) {
+       ArrayList<Integer> result = new ArrayList<>();
+       Arrays.sort(arr);
+       for(int i = 1; i < n  ; i++){
+           if(arr[i-1] == arr[i]){
+               result.add(arr[i]);
+            }
+       }
+       Set<Integer> st = new HashSet<>t(result);
+       result.clear();
+       result.addAll(st);
+       Collections.sort(result);
+       if(result.size()<1) result.add(-1);
+       return result;
+    }
+}
+}
