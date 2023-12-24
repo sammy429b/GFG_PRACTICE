b7e48e554340fe6192f57cfb2dea364a015bf25d
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-12-24 14:41:15 +0530

    Union of two arrays

---
+++ DSA/Array/Basic/Union_of_two_arrays.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+]
+public class Union_of_two_arrays {
+    class Solution {
+    public static int doUnion(int a[], int n, int b[], int m) {
+        Set<Integer> unionSet = new HashSet<>();
+        
+        for (int i = 0; i < n; i++) {
+            unionSet.add(a[i]);
+        }
+        
+        for (int j = 0; j < m; j++) {
+            unionSet.add(b[j]);
+        }
+        
+        return unionSet.size();
+    }
+}
+}
